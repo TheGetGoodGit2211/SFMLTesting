cdb12707f5f724afd54548bf56e549384b6ac05b
TheGetGoodGit2211 <jaymideansheldon@gmail.com>
2024-07-14 19:55:31 +0100

    Tried rendering the test.png image.

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,44 @@
+#include "SFML/Window/VideoMode.hpp"
 #include "SFML/Window/Window.hpp"
 #include <SFML/Graphics.hpp>
 
 int main()
 {
-  sf::RenderWindow window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
+  
+  unsigned int winWidth = 500;
+  unsigned int winHeight = 500;
+  const char* winTitle = "SFMLTesting";
+
+  sf::RenderWindow window = sf::RenderWindow{sf::VideoMode(winWidth, winHeight), winTitle};
   window.setFramerateLimit(144);
+
+  const char* imgPath = "./sprites/test.png";
+  sf::Texture texture; texture.loadFromFile(imgPath);
+
+  sf::Sprite sprite; sprite.setTexture(texture);
+
+  sprite.scale(5.f, 5.f);
 
   while (window.isOpen())
   {
+
     sf::Event event;
     while (window.pollEvent(event))
     {
+
       if (event.type == sf::Event::Closed)
       {
+
         window.close();
+
       }
+
     }
 
-    window.clear();
+    window.clear(sf::Color::White);
+    window.draw(sprite);
     window.display();
+
   }
+
 }
